f8c54dc86e69ded20f6f37e791286494c0c21641
Haydon <26003762+HAYD323@users.noreply.github.com>
2024-08-05 19:49:06 +0100

    refactored app code

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-const { deepStrictEqual } = require('assert');
 const fs = require('fs');
 const path = require('path');
 const csv = require('fast-csv');
 const dy = require('./src/dyParser');
 
 async function feed1() {
-    const feed =[];
-    return new Promise((resolve,reject)=>{
-        fs.createReadStream(path.resolve(__dirname,'assets',dy.feed1FileName.toLowerCase().replace('.csv','')+'.csv'))
+    const feed = [];
+    return new Promise((resolve, reject) => {
+        let fileName = dy.feed1FileName.toLowerCase().replace('.csv','') + '.csv';
+
+        fs.createReadStream(path.resolve(__dirname,'assets',fileName))
         .pipe(csv.parse({ headers: true }))
         .on('data', row => {feed.push(row);})
         .on('error', (error) => reject(error))
@@ -16,9 +17,11 @@ async function feed1() {
 }
 
 async function feed2() {
-    const feed =[];
-    return new Promise((resolve,reject)=>{
-        fs.createReadStream(path.resolve(__dirname,'assets',dy.feed2FileName.toLowerCase().replace('.csv','')+'.csv'))
+    const feed = [];
+    return new Promise((resolve, reject) => {
+        let fileName = dy.feed2FileName.toLowerCase().replace('.csv','') + '.csv';
+
+        fs.createReadStream(path.resolve(__dirname,'assets',fileName))
         .pipe(csv.parse({ headers: true }))
         .on('data', row => {feed.push(row);})
         .on('error', (error) => reject(error))
@@ -26,11 +29,23 @@ async function feed2() {
     });
 }
 
-Promise.allSettled([feed1(),feed2()]).then(res=>{
+Promise.allSettled([feed1(), feed2()]).then(([feed1, feed2]) => {
     try {
-        let outputJson = dy.parser(res[0].value,res[1].value);
-        console.log('item count:',outputJson.length);
-        fs.writeFileSync(path.resolve(__dirname,'logs','log.json'),JSON.stringify(outputJson).replaceAll("},","},\n"),'utf8');
+        let outputJson = dy.parser(feed1.value, feed2.value);
+        console.log('item count:', outputJson.length);
+        fs.writeFileSync(
+            path.resolve(
+                __dirname,
+                'logs',
+                'log.json'
+            ),
+            JSON.stringify(
+                outputJson,
+                null,
+                2
+            ),
+            'utf8'
+        );
     } catch (err) {
         console.log('ERROR:',err);
     }
